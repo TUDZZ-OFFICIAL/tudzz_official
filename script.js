e599766bbafca4b6321194d4ff941ae39dcc70bc
JANAKIRAMAN <janakiramankeerthivelan1021@gmail.com>
2023-12-22 18:30:22 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// You can add JavaScript functionality here if needed
+// For example, smooth scrolling to sections when clicking on links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
